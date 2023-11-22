17f75e01d56b1c0d02ce23233b153dd606f9a244
optimum10 <100962824+optimum10@users.noreply.github.com>
2023-11-22 15:57:37 +0530

    Search backend working and complete frontend improvement left

--- Login_Server/src/index.js
+++ Login_Server/src/index.js
@@ -5,10 +5,11 @@ const bcrypt = require('bcrypt');
 // const router = require('express').Router() ; 
 const session = require('express-session') ; 
 const passport = require('passport');
-// const passport  = require('x') ; 
+const morgan = require('morgan')  ; 
+// const passport  = require('x') ;       
 require('./auth') ;         
-//const{collection,searchcollection} = require('./config') ;
 const{collection} = require('./config') ; 
+// const Blog = require('./config') ; 
 
 const app = express() ;                               
 app.use(express.json())  // conversion to jsonformat  
@@ -17,20 +18,30 @@ app.use(express.static(path.join(__dirname,'client')))
 app.use(express.urlencoded({extended: false}));
 
 app.set('view engine','ejs'); 
-app.use(express.static("public")) ;
+app.use(express.static('public')) ;
 
 var mongoose = require('mongoose');
-const BlogsSchema = new mongoose.Schema({
-                        Topic:String,
-                        Title:String,
-                        Content:String,
-                    }) ;
- console.log(" before model creation ") ;                     
+// const BlogsSchema = new mongoose.Schema({
+//                         Topic:String,
+//                         Title:String,
+//                         Snippet:String,
+//                         Body:String,
+//                     }) ;
+//  console.log(" before model creation ") ;                     
  
 
- const blogsmodel =  new mongoose.model('Blogs',BlogsSchema) ;
- 
+//  const blogsmodel =  mongoose.model('Blog') ;
+// const Blog = require('./config').Blog ; 
 
+const blogSchema=new mongoose.Schema({
+      title:{type:String, required:true},
+      snippet:{type:String, required:true},
+      body:{type:String, required:true}
+  },{timestamps:true});
+  
+
+ 
+  const Blog = mongoose.model('Blog', blogSchema);
 
 
 function isLoggedIn(req,res,next){
@@ -61,6 +72,7 @@ app.get('/auth/google/callback',
       successRedirect: '/auth/protected',
       failureRedirect: '/auth/google/failure' 
 }));
+
 //   function(req, res) {
 //     // Successful authentication, redirect home.
 //     res.redirect('/');
@@ -72,7 +84,7 @@ app.get('/auth/google/failure', (req,res)=>{
 
 app.get('/auth/protected',isLoggedIn,(req,res)=>{
        let name = req.user.displayName ;
-       res.send(`Hello ${name}`) ;  
+       res.redirect('http://localhost:300/home') ;  
 }) ; 
 
 app.use('/auth/logout',(req,res) =>{
@@ -89,8 +101,31 @@ app.get('/signup',(req,res) => {
        res.render('signup') ; 
 }); 
 
+app.get('/about',(req,res)=>{ 
+      res.render('about' , {title:'About'})
+  })
+  
+  app.get('/blog/create', (req,res) =>{
+      res.render('create',{title:'Create a new Blog'})
+  })
+  
+  app.get('/home', (req,res) =>{
+      res.render('home',{title:'Create a new Blog'})
+  })
+  
+  app.get('/blogs', (req,res)=>{
+      Blog.find().sort({createdAt:-1})
+      .then((result)=>{
+          res.render('index', {title: 'ALL Blogs', blogs: result})
+      })
+      .catch((err)=>{
+          console.log(err);
+      })
+})
+  
+
 // app.get('/login',(req,res) => {
-//        res.redirect('http://localhost:300/home') ; 
+//        res.render('home') ; 
 // })
 
 
@@ -124,15 +159,14 @@ app.post('/signup', async (req,res)=>{
 app.post("/login",async (req,res) =>{
        try{
              const checkUser = await collection.findOne({name: req.body.username});
-             const checkEmail = await collection.findOne({name: req.body.email}) ; 
+             const checkEmail = await collection.findOne({email: req.body.email}) ; 
              if(!checkUser || !checkEmail){
                    res.send("user not found") ; 
              }
              const isPassMatch = await bcrypt.compare(req.body.password,checkUser.password);
              if(isPassMatch){
-                  app.get('/login',(req,res) => {
-                        res.redirect('http://localhost:300/home') ; 
-                 })
+                       res.redirect('/login') ; 
+                       return ; 
              }
              else{
                   req.send("wrong password");
@@ -143,34 +177,39 @@ app.post("/login",async (req,res) =>{
        
 })
 
+app.get('/login',(req,res) => {
+      res.redirect('/home') ; 
+})
+
 
 app.post("/search",async(req,res)=>{
-      try{
+      try{                              
             
         console.log(" in search method ") ;
-        const midstringtopic = req.body.SearchString;
-        const midstringtitle = req.body.SearchString;
-        const midstringcontent = req.body.SearchString;
+        console.log(req.body) ; 
+
+        const midstringtitle = req.body.searchInput ;
+        const midstringsnippet = req.body.searchInput ;  
+        const midstringbody = req.body.searchInput ;
 
         console.log(" before 3 regex patterns");
-        const regexPattern = new RegExp(`.*${midstringtopic}.*`,'i') ;   
-        const regexPattern1 = new RegExp(`.*${midstringtitle}.*`,'i') ;       
-        const regexPattern2 = new RegExp(`.*${midstringcontent}.*`,'i') ;
-
-        existingTopic = await blogsmodel.find({ Topic : {$regex: regexPattern }}) ;
-        existingTitle = await blogsmodel.find({Title : {$regex: regexPattern1 }}) ; 
-        existingContent = await blogsmodel.find({ Content :  {$regex: regexPattern2 }}) ;  
+        const regexPattern = new RegExp(`.*${midstringtitle}.*`,'i') ;   
+        const regexPattern1 = new RegExp(`.*${midstringsnippet}.*`,'i') ;       
+        const regexPattern2 = new RegExp(`.*${midstringbody}.*`,'i') ; 
+                                                                                                         
+        existingTitle = await Blog.find({ title : {$regex: regexPattern }}) ;
+        existingSnippet = await Blog.find({snippet : {$regex: regexPattern1 }}) ; 
+        existingBody = await Blog.find({ body :  {$regex: regexPattern2 }}) ;  
 3
-        const combinedResults = existingTopic.concat(existingTitle, existingContent);
-        console.log(" map print ");
-        console.log(Array.from(combinedResults.map(id => combinedResults.find(result => result._id === id))));
+        const combinedResults = existingTitle.concat(existingSnippet , existingBody);
+      //   console.log(" map print ");
+      //   console.log(Array.from(combinedResults.map(id => combinedResults.find(result => result._id === id))));
         // Remove duplicates based on a unique identifier (e.g., _id)
         const uniqueResults = Array.from(new Set(combinedResults.map(result => result._id)))
                                    .map(id => combinedResults.find(result => result._id === id));
-       
        if(uniqueResults){
          console.log(uniqueResults);
-         res.json(uniqueResults); // Send the actual data from the database
+         res.send(uniqueResults); // Send the actual data from the database
       }
       else {
             console.log("Topic could not be found");
@@ -184,29 +223,71 @@ app.post("/search",async(req,res)=>{
       //mongoose.connection.close();
 })
 
-app.post("/createblog",async(req,res)=>{
-      try{
+
+app.post('/blogs',(req,res)=>{
+      const blog=new Blog(req.body) //creating a new instance of Blog schema and passing the submitted data in form of req.body
+      blog.save() //saving it to the database
+      .then((result)=>{
+          res.redirect('/blogs');
+      })
+      .catch((err)=>{
+          console.log(err);
+      })
+  })
+
+
+//   app.get('/search',(req,res))
+
+
+  app.get('/blogs/:id',(req,res)=>{
+      const id=req.params.id;
+      Blog.findById(id)
+      .then(result=>{
+          res.render('details', {blog : result , title:'Blog Details'})
+      })
+      .catch(err=>{
+          console.log(err);
+      })
+  
+  })
+  app.delete('/blogs/:id',(req,res)=>{
+      const id=req.params.id;
+      Blog.findByIdAndDelete(id)
+      .then(result=>{
+          res.json( {redirect:  '/blogs' })
+      })
+      .catch(err=>{
+          console.log(err);
+      })
+  })
+  
+  app.use((req,res)=>{
+      res.status(404).render('404',{title: '404'})
+  })
+  
+  
+
+// app.post("/createblog",async(req,res)=>{
+//       try{
             
-          console.log(" in  create blog method ") ;
+//           console.log(" in  create blog method ") ;
          
        
-          const newblog = new blogsmodel({ Topic: req.body.Topic,
-                                           Title: req.body.Title, 
-                                           Content: req.body.Content 
-                                         }); 
-                                         console.log(" newblog created") ;                               
-          newblog.save();
-          console.log("new blog saved" );
-          res.json(newblog);
-    }
-      catch{
-            console.error("Error in creating blog:");
-            res.status(500).send("Internal Server Error");
-      }
-     // mongoose.connection.close();
-}) 
-
-
+//           const newblog = new blogsmodel({ Topic: req.body.Topic,
+//                                            Title: req.body.Title, 
+//                                            Content: req.body.Content 
+//                                          }); 
+//                                          console.log(" newblog created") ;                               
+//           newblog.save();
+//           console.log("new blog saved" );
+//           res.json(newblog);
+//     }
+//       catch{
+//             console.error("Error in creating blog:");
+//             res.status(500).send("Internal Server Error");
+//       }
+//      // mongoose.connection.close();
+// }) 
 
 const port = 5000 ; 
 app.listen(port,() =>{
@@ -258,9 +339,9 @@ app.listen(port,() =>{
 //       })
 //     );
 
-app.get('/auth/google/failure', (req,res)=>{
-      res.send('Authentication Failed !') ; 
-}) ; 
+// app.get('/auth/google/failure', (req,res)=>{
+//       res.send('Authentication Failed !') ; 
+// }) ; 
 
 
 
