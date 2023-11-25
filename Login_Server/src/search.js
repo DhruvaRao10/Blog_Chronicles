18bad4e0a39e493707a4adef95cb633398df0cd5
optimum10 <100962824+optimum10@users.noreply.github.com>
2023-11-25 10:14:35 +0530

    Project intgrated

--- Login_Server/src/search.js
+++ Login_Server/src/search.js
@@ -1,43 +1,43 @@
-function toggleSearch() {
-    var expand = document.getElementById("searchExpand");
-    var text = document.getElementById("searchText");
-    text.classList.toggle("expanded");
-    expand.classList.toggle("expanded");
-    if (text.classList.contains("expanded")) {
-      text.textContent = "×";
-    } else {
-      text.textContent = "Search";
-    }
-  }
+// function toggleSearch() {
+//     var expand = document.getElementById("searchExpand");
+//     var text = document.getElementById("searchText");
+//     text.classList.toggle("expanded");
+//     expand.classList.toggle("expanded");
+//     if (text.classList.contains("expanded")) {
+//       text.textContent = "×";
+//     } else {
+//       text.textContent = "Search";
+//     }
+//   }
   
-  async function submitSearch() {
-    var searchTerm = document.getElementById("searchInput").value;
+//   async function submitSearch() {
+//     var searchTerm = document.getElementById("searchInput").value;
   
-    try {
-      const response = await fetch('/search', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ SearchString: searchTerm }),
-      });
+//     try {
+//       const response = await fetch('/search', {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({ SearchString: searchTerm }),
+//       });
 
-      console.log(response);
+//       console.log(response);
   
-      if (response.ok) {
-        const data = await response.json();
-        console.log("Search results:", data);
-        // Update your UI with the search results if needed
-      } else {
-        console.error("Search request failed:", response.statusText);
-      }
-    } catch (error) {
-      console.error("Error during search request:", error);
-    }
-  }
+//       if (response.ok) {
+//         const data = await response.json();
+//         console.log("Search results:", data);
+//         // Update your UI with the search results if needed
+//       } else {
+//         console.error("Search request failed:", response.statusText);
+//       }
+//     } catch (error) {
+//       console.error("Error during search request:", error);
+//     }
+//   }
 
-  window.toggleSearch = toggleSearch ; 
-  window.submitSearch = submitSearch ; 
+//   window.toggleSearch = toggleSearch ; 
+//   window.submitSearch = submitSearch ; 
   
 
 
